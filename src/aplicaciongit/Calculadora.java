b2210d26b11fb3e51b2776e592db4c07536cfb1d
nicol <nicol@LauraNicols>
2025-05-29 11:43:06 +0200

    Agregada la clase Calculadora

---
+++ src/aplicaciongit/Calculadora.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aplicaciongit;
+
+/**
+ *
+ * @author nicol
+ */
+public class Calculadora {
+
+    public int suma(int a, int b) {
+        return a + b;
+    }
+}
